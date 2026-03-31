c38fdf0a66d15902dde26fd3c56c50069992fd52
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-03-31 13:00:59 +0530

    controller house keeping task

---
+++ app/controllers/housekeeping_task_controller.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from app.models.housekeeping_task import HouseKeepingTask
+# from app.schemas.housekeeping_task_schema import TaskCreate, TaskUpdate
+
+
+#--------Get all------------------------------------
+def get_tasks(db: Session):
+    return db.query(HouseKeepingTask).all()
+
+
+#------------create-------------------
+def create_task(data, db: Session):
+    # object creation
+    task = HouseKeepingTask(**data.dict())
+
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
+#------------get by id -------------------
+def get_task(id: int, data, db: Session):
+    task = db.query(HouseKeepingTask).filter(HouseKeepingTask.id == id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task
+
+#-------------------update----------------------
+def update_task(id: int, data, db: Session):
+    task = db.query(HouseKeepingTask).filter(HouseKeepingTask.id == id).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    for key, value in data.dict().items():
+        setattr(task, key, value)
+    
+    db.commit()
+    db.refresh(task)
+    return task
+
+
+#--------------delete-----------
+def delete_task(id: int, db: Session):
+    task = db.query(HouseKeepingTask).filter(HouseKeepingTask.id == id).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found.")
+
+    db.delete(task)
+    db.commit()
+
+    return {"message": "Deleted successfully."}
+    
