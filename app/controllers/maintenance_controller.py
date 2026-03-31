df3aa631bc76ddd73e8a4b52102ea38592f3cc54
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-03-31 15:33:20 +0530

    maintenance request

---
+++ app/controllers/maintenance_controller.py
@@ -0,0 +1,58 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from app.models.maintenance_request import MaintenanceRequest
+
+
+#-------------Get all------------------
+def get_requests(db: Session):
+    return db.query(MaintenanceRequest).all()
+
+#-------------create-------------
+def create_request(db: Session, data):
+    obj = MaintenanceRequest(**data.dict())
+
+    db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
+
+
+#-------get by id ----------
+def get_request(id: int, db: Session):
+    obj = db.query(MaintenanceRequest).filter(MaintenanceRequest.id == id).first()
+
+    if not obj:
+        raise HTTPException(status_code=404, detail="request not found")
+    
+    return obj
+
+
+
+#-------update---------
+def update_request(id: int, data, db: Session):
+    obj = db.query(MaintenanceRequest).filter(MaintenanceRequest.id == id).first()
+
+    if not obj:
+        raise HTTPException(status_code=404, detail="Request not found.")
+
+    for key, value in data.dict().items():
+        setattr(obj, key, value)
+    
+    db.commit()
+    db.refresh(obj)
+    return obj
+
+
+
+
+#------------------delete---------------------
+def delete_request(id: int, db: Session):
+    obj = db.query(MaintenanceRequest).filter(MaintenanceRequest.id == id).first()
+
+    if not obj:
+        raise HTTPException(status_code=404, detail="Request not found.")
+
+    obj.status = status
+    db.commit()
+
+    return obj
