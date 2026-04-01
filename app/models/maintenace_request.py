dbb449a65e4bd7577c10e1de0612e43d50de10d4
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-01 13:48:06 +0530

    models

---
+++ app/models/maintenace_request.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from app.models.base import BaseModel
+
+
+class MaintenanceRequest(BaseModel):
+    __tablename__ = "maintenance_requests"
+
+
+    room_id = Column(Integer, ForeignKey("rooms.id"), nullable=False)
+    reported_by = Column(Integer, ForeignKey("rooms.id"), nullable=False)
+
+    issue = Column(Text, nullable=False)
+    status = Column(String(50), default="pending")
+
+    # room = relationship("Room", back_populates="maintenance_requests")
+    # reporter = relationship("User", back_populates="maintenance_requests")
+
+    # def __repr__(self):
+    #     return f"<MaintenanceRequest {self.id} - {self.status}>"
+    
