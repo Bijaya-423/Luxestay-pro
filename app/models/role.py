59cb9efb93227057bed9a3a6b119712dae43598f
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-07 19:28:28 +0530

    models

---
+++ app/models/role.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column , Integer, String, DateTime
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    created_by = Column(Integer, default=0)
+
+
+    
