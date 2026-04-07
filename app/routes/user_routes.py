538513cb4bf077ccf2c284c242bdac1d0527b2a2
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-07 19:29:06 +0530

    routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.controllers.user_controller import assign_role
+from app.core.database import get_db
+from app.dependencies.auth_dependency import get_current_user
 
 router = APIRouter()
 
-@router.get("/")
-def get_users():
-    return {"message": "Users API working ✅"}
+@router.post("/assign-role")
+def assign_role_api(user_id: int, role_id: int, db: Session = Depends(get_db), user = Depends(get_current_user)):
+    return assign_role(user_id, role_id, db, user)
+    
