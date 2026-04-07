7e9850465e96ae2735b3e24ea750ca85525652fd
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-07 19:26:40 +0530

    auth

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,42 +1,102 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from app.models.user import User
+from app.models.role import Role
+from app.models.user_role import UserRole
+from app.models.employee import Employee
 from app.core.security import hash_password, verify_password, create_access_token
 
 
-# REGISTER USER
 def register_user(data, db: Session):
-    
-    # Check if user already exists
+    #check duplicate email
     existing_user = db.query(User).filter(User.email == data.email).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    # Check if first user (Admin)
+        raise HTTPException(status_code=400, detail="Email already exists.")
+    
+    #check total users
     total_users = db.query(User).count()
 
-    user_type = "Admin" if total_users == 0 else "Employee"
+    #first user ->  admin
+    if total_users == 0:
+        admin_role = db.query(Role).filter(Role.name == 'admin').first()
 
+        if not admin_role:
+            raise HTTPException(status_code=500, detail="Admin role not found.")
+        
+        user_type = 'admin'
+        created_by = 0
+    else:
+        user_type = 'employee'
+        # created_by = 1
+
+        # 🔥 get admin dynamically
+        admin_user = db.query(User).filter(User.type == 'admin').first()
+        
+        if not admin_user:
+            raise HTTPException(status_code=500, detail="Admin user not found.")
+
+        created_by = admin_user.id
+        # created_by = admin_user.id if admin_user else 1
+
+    
+    #create user
     new_user = User(
-        name=data.name,
-        email=data.email,
-        password=hash_password(data.password),
-        type=user_type,
-        created_by=0 if total_users == 0 else 1
-    )
+        name = data.name,
+        email = data.email,
+        password = hash_password(data.password),
+        type = user_type,
+        created_by = created_by
 
+    )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    
+
+    if total_users == 0:
+        role_id = admin_role.id
+
+    else:
+        default_role = db.query(Role).filter(Role.name == 'employee').first()
+        if not default_role:
+            raise HTTPException(status_code=500, detail="Default Employee Role Not found.")
+        role_id = default_role.id
+
+    user_role = UserRole(
+        user_id = new_user.id,
+        role_id=role_id
+    )
+    db.add(user_role)
+    db.commit()
+
+
+    # 6. CREATE EMPLOYEE (ONLY NON-ADMIN USERS)
+    if total_users != 0:
+        employee = Employee(
+            user_id=new_user.id,
+            name=data.name,
+            email=data.email,
+            password=new_user.password,
+            employee_id=f"EMP{new_user.id}",
+            branch_id=1,          # TODO: dynamic later
+            department_id=1,      # TODO: dynamic later
+            designation_id=1,     # TODO: dynamic later
+            created_by=created_by
+        )
+
+        db.add(employee)
+        db.commit()
 
     return {
-        "message": f"{user_type} registered successfully",
+        "message": f"{user_type} registered successfully.",
         "user_id": new_user.id
     }
 
 
+
 # LOGIN USER
 def login_user(data, db: Session):
+
 
     user = db.query(User).filter(User.email == data.email).first()
 
