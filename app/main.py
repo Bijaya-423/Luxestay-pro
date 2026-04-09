c2a6e3a915d15db8cd43f2b8e018d95fcc695aa3
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-09 21:46:29 +0530

    main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,43 @@
-# Entry point of FastAPI app
+# # Entry point of FastAPI app
+
+# from fastapi import FastAPI
+# from app.routes.router import api_router
+# from app.core.database import Base, engine
+# from mangum import Mangum
+
+# # Create tables
+# Base.metadata.create_all(bind=engine)
+
+# app = FastAPI(title="IFAHMS API")
+
+# app.include_router(api_router, prefix="/api")
+
+# handler = Mangum(app, api_gateway_base_path="/ifahms-lambda")
 
 from fastapi import FastAPI
 from app.routes.router import api_router
-from app.core.database import Base, engine
+from mangum import Mangum
 
-# Create tables
-Base.metadata.create_all(bind=engine)
+# from app.core.database import Base, engine
+# Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="IFAHMS API")
+
+app = FastAPI(
+    title="IFAHMS API",
+    root_path="/default/ifahms-lambda"
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "API WORKING 🚀"}
+
+@app.get("/test")
+def test():
+    return {"ok": True}
+
 
 app.include_router(api_router, prefix="/api")
 
-# @app.get("/")
-# def root():
-#     return {"message": "IFAHMS Backend Running 🚀"}
+handler = Mangum(app, api_gateway_base_path="/default/ifahms-lambda")
+
