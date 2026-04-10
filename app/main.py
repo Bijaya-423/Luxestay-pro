7f9c2f995d9ae05480ef57763a548051d10de33e
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-10 22:51:23 +0530

    main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,39 @@
+# from fastapi import FastAPI
+# from app.routes.router import api_router
+# from mangum import Mangum
+
+# # from app.core.database import Base, engine
+# # Base.metadata.create_all(bind=engine)
+
+
+# app = FastAPI(
+#     title="IFAHMS API",
+#     root_path="/default/ifahms-lambda"
+# )
+
+
+# @app.get("/")
+# def root():
+#     return {"message": "API WORKING 🚀"}
+
+# @app.get("/test")
+# def test():
+#     return {"ok": True}
+
+
+# app.include_router(api_router, prefix="/api")
+
+# handler = Mangum(app, api_gateway_base_path="/default/ifahms-lambda")
+
 from fastapi import FastAPI
 from app.routes.router import api_router
 from mangum import Mangum
 
-# from app.core.database import Base, engine
-# Base.metadata.create_all(bind=engine)
-
-
 app = FastAPI(
     title="IFAHMS API",
-    root_path="/default/ifahms-lambda"
+    docs_url="/docs",
+    redoc_url="/redoc"
 )
-
 
 @app.get("/")
 def root():
@@ -20,8 +43,6 @@ def root():
 def test():
     return {"ok": True}
 
-
 app.include_router(api_router, prefix="/api")
 
-handler = Mangum(app, api_gateway_base_path="/default/ifahms-lambda")
-
+handler = Mangum(app, lifespan="off")
