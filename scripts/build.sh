cc033625ce9cec14283793521546dfcf006e66aa
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-13 11:26:16 +0530

    fix: faster build - remove buildx, merge ecr login

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
+# set -e
+
+
+# echo ">>> Staring Docker Build...."
+# echo "    Image: ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG}"
+
+
+# #create or reuse buildx builder
+# docker buildx create --name lambdabuilder --use 2>/dev/null || \
+#     docker buildx use lambdabuilder
+
+
+#     # #build and push image
+#     # docker buildx build \
+#     # --platform linux/amd64 \
+#     # --tag ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG} \
+#     # --push \
+#     # .
+
+# docker buildx inspect --bootstrap
+
+# #Build linux/amd64 image and push directly to ECR
+# # --provenance=false prevents OCI manifest that lambda rejects
+
+# docker buildx build \
+#     --platform linux/amd64 \
+#     --provenance=false \
+#     -t ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG} \
+#     -t ${ECR_REGISTRY}/${ECR_REPO_NAME}:latest \
+#     --push \
+
+# echo ">>> Build and push Complete"
+
+
 set -e
 
+echo ">>> Logging into ECR..."
+aws ecr get-login-password --region ${AWS_REGION} | \
+    docker login --username AWS --password-stdin ${ECR_REGISTRY}
 
-echo ">>> Staring Docker Build...."
-echo "    Image: ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG}"
-
-
-#create or reuse buildx builder
-docker buildx create --name lambdabuilder --use 2>/dev/null || \
-    docker buildx use lambdabuilder
-
-
-    # #build and push image
-    # docker buildx build \
-    # --platform linux/amd64 \
-    # --tag ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG} \
-    # --push \
-    # .
-
-docker buildx inspect --bootstrap
-
-#Build linux/amd64 image and push directly to ECR
-# --provenance=false prevents OCI manifest that lambda rejects
-
-docker buildx build \
+echo ">>> Building image..."
+docker build \
     --platform linux/amd64 \
-    --provenance=false \
     -t ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG} \
     -t ${ECR_REGISTRY}/${ECR_REPO_NAME}:latest \
-    --push \
+    .
 
-echo ">>> Build and push Complete"
+echo ">>> Pushing to ECR..."
+docker push ${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG}
+docker push ${ECR_REGISTRY}/${ECR_REPO_NAME}:latest
+
+
+echo ">>> Build and Push complete"
