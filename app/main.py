3947c02d0a1147901fed284d843e67e7d9582e9d
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-04-25 21:11:18 +0530

    changes main.py file

--- app/main.py
+++ app/main.py
@@ -1,29 +1,3 @@
-# from fastapi import FastAPI
-# from app.routes.router import api_router
-# from mangum import Mangum
-
-# # from app.core.database import Base, engine
-# # Base.metadata.create_all(bind=engine)
-
-
-# app = FastAPI(
-#     title="IFAHMS API",
-#     root_path="/default/ifahms-lambda"
-# )
-
-
-# @app.get("/")
-# def root():
-#     return {"message": "API WORKING 🚀"}
-
-# @app.get("/test")
-# def test():
-#     return {"ok": True}
-
-
-# app.include_router(api_router, prefix="/api")
-
-# handler = Mangum(app, api_gateway_base_path="/default/ifahms-lambda")
 
 from fastapi import FastAPI
 from app.routes.router import api_router
